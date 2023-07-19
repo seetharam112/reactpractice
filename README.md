9a5dfc61bbf0dbfcfdbc2453eefdfee3f8ebf153
Seetharam <you@example.com>
2023-07-19 13:02:42 +0530

    episode igniting app

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Surya's kitchen
+
+# parcel
+- minify 
+- bundling
+- tree shaking
+- package managing
+- hot reloading
+- create local server and deploy app
--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement('div', { id: "parent", className: "parent" }, [
     React.createElement('div', { id: "child", className: "child" }, [
         React.createElement('h1', { id: "", className: "" }, "Welcome to surya's kitchen"),
